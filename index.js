15d5e1227e91b6c8312c0f049fc15d2717427839
vietdung96 <102815340+vietdung96@users.noreply.github.com>
2022-08-16 19:38:01 +0700

    gio hang + thanhtoan

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 1234;
+const port =process.env.PORT | 1234;
 app.listen(port, function () {
 	console.log('server is running..');
 });
@@ -10,11 +10,14 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
 	res.render('makeup.ejs');
 });
+app.set('view engine', 'ejs');
+app.use(express.static('public')); //cho phép truy cập vào các static
+
 app.get('/skincare/face', function (req, res) {
 	res.render('face');
 });
 app.get('/skincare/body-care', function (req, res) {
-	res.render('body-care.ejs');
+	res.render('body-care');
 });
 app.get('/skincare/men', function (req, res) {
 	res.render('men');
@@ -27,4 +30,10 @@ app.get('/makeup/eyes', function (req, res) {
 });
 app.get('/makeup/lips', function (req, res) {
 	res.render('make-lips');
+});
+app.get('/shipping',function (req, res){
+	res.render('thanhtoan');
+});
+app.get('/shopping',function (req, res){
+	res.render('shop-ping');
 });
