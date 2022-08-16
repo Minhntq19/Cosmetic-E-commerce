c733d768d7cb4eae3e1a8d3889e0dd6fc2bf2097
Minhntq19 <102934112+Minhntq19@users.noreply.github.com>
2022-08-16 10:41:51 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const port = 1234;
@@ -12,34 +11,32 @@ app.get('/', (req, res) => {
 	res.render('makeup.ejs');
 });
 app.get('/', (req, res) => {});
-=======
-const express = require("express");
-const  app = express();
+const express = require('express');
+const app = express();
 const port = process.env.PORT | 1108;
 
-app.listen(port,function () {
-    console.log("đang ỉa chảy chạy cái l**");
+app.listen(port, function () {
+	console.log('đang ỉa chảy chạy cái l**');
 });
 
-app.set("view engine","ejs");
-app.use(express.static("public"));//cho phép truy cập vào các static
+app.set('view engine', 'ejs');
+app.use(express.static('public')); //cho phép truy cập vào các static
 
-app.get("/skincare/face",function (req, res){
-    res.render("face");
+app.get('/skincare/face', function (req, res) {
+	res.render('face');
 });
-app.get("/skincare/body-care",function (req, res){
-    res.render("body-care");
+app.get('/skincare/body-care', function (req, res) {
+	res.render('body-care');
 });
-app.get("/skincare/men",function (req, res){
-    res.render("men");
+app.get('/skincare/men', function (req, res) {
+	res.render('men');
 });
-app.get("/makeup/face",function (req, res){
-    res.render("make-face");
+app.get('/makeup/face', function (req, res) {
+	res.render('make-face');
 });
-app.get("/makeup/eyes",function (req, res){
-    res.render("make-eyes");
+app.get('/makeup/eyes', function (req, res) {
+	res.render('make-eyes');
 });
-app.get("/makeup/lips",function (req, res){
-    res.render("make-lips");
+app.get('/makeup/lips', function (req, res) {
+	res.render('make-lips');
 });
->>>>>>> bfe67846739b62b233ac5e14edace377c1708cd9
